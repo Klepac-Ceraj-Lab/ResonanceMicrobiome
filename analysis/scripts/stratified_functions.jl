87ab9852a743f1042036ef03a131f4ca08cc321b
Kevin Bonham <kevbonham@gmail.com>
2020-03-19 13:39:16 -0400

    more tweaks to figure3

--- analysis/scripts/stratified_functions.jl
+++ analysis/scripts/stratified_functions.jl
@@ -1,16 +1,5 @@
-using Pkg
-Pkg.activate(".")
-using ECHOAnalysis
-using DataFrames
-using CSV
-using SQLite
-using Microbiome
-using PrettyTables
-import Pkg.TOML: parsefile
-using MakieLayout
-using Makie
-using StatsMakie
 using Colors
+import StatsMakie.StructArrays: uniquesorted
 
 kodb = SQLite.DB("~/Desktop/ko_profiles.sqlite")
 allmeta = getmgxmetadata("/Users/ksb/Desktop/metadata.sqlite")
@@ -18,32 +7,127 @@ allmeta = getmgxmetadata("/Users/ksb/Desktop/metadata.sqlite",samples=uniquetime
 include("accessories.jl")
 # add_functional_profiles(kodb, "data/engaging", stratified=true, kind="kos_relab")
 
-koslong = DataFrame(DBInterface.execute(kodb, "SELECT * FROM kos_relab"))
+koslong = DataFrame(DBInterface.execute(kodb, "SELECT * FROM ko_names_relab"))
 nko = get_neuroactive_kos()
 
 # replace this with `select!()` once it's added https://github.com/JuliaData/DataFrames.jl/pull/2080
+
 koslong = hcat(koslong, DataFrame(map(eachrow(koslong)) do row
-    m = match(r"^(\w+)\|?(?:(?:g__([\w]+)\.s__([\w]+))?|(unclassified))$", row.function)
+    m = match(r"^(.+?)\|?(?:(?:g__([\w]+)\.s__([\w]+))?|(unclassified))$", row.function)
+    isnothing(m) && @show row.function
     (ko, genus, species, unclass) = m.captures
     if isnothing(genus) && isnothing(unclass)
         ko == row.function || error("Weird function $(row.function)")
         taxon = nothing
     else
-        isnothing(unclass) ? taxon = "$genus $species" : taxon = "unclassified"
+        isnothing(unclass) ? taxon = "$species" : taxon = "unclassified"
     end
-
+    ko = split(ko, ':')[1]
     (ko=ko, taxon=taxon)
     end
 ))
-filter!(row-> !ismissing(row.ageLabel), allmeta)
+
+filter!(row-> !ismissing(row.ageLabel) && row.ageLabel != "mom", allmeta)
+filter!(row-> !ismissing(row.cogScore), allmeta)
+
 let samples = Set(allmeta.sample)
     filter!(row-> row.sample in samples, koslong)
 end
-
+length(unique(koslong.sample))
 agelabels = dictionary(sample=>age for (sample, age) in eachrow(allmeta[!, [:sample, :ageLabel]]))
 koslong.agelabel = [agelabels[s] for s in koslong.sample]
+cogscores = dictionary(sample=>score for (sample, score) in eachrow(allmeta[!, [:sample, :cogScore]]))
+koslong.cogScore = [cogscores[s] for s in koslong.sample]
 
-(s,t) = let geneset = nko["Glutamate degradation"]
+##
+
+gluts = let geneset = Set(nko["Glutamate synthesis"])
+    filt = filter(row-> row.ko in geneset, koslong)
+    samples = by(filt, [:sample, :taxon]) do sample
+        (total=sum(sample.abundance), agelabel=first(sample).agelabel, cogScore=first(sample).cogScore)
+    end
+
+    sort!(samples, [:agelabel, :sample])
+    totals = filter(row-> isnothing(row.taxon), samples)
+    filter!(row-> !isnothing(row.taxon), samples)
+    totals.ord = invperm(sortperm(totals, [:agelabel, :total]))
+
+    spectotals = by(samples, :taxon, spectotal = :total =>sum)
+    sort!(spectotals, :spectotal)
+    topspec = Set(last(spectotals, 5).taxon)
+
+    by(samples, :sample) do df
+        df = filter(row-> !in(row.taxon, topspec), df)
+        if size(df, 1) == 0
+            1
+        else
+            other = sum(df.total)
+            sample = first(df).sample
+            push!(samples, (sample=sample, taxon="other", total=other, agelabel=agelabels[sample], cogScore=cogscores[sample]))
+            1
+        end
+    end
+    push!(topspec, "other")
+    filter!(row-> row.taxon in topspec, samples)
+
+
+    for s in unique(samples.sample)
+        missing_tax = setdiff(topspec, samples[samples.sample .== s, :taxon])
+        for t in missing_tax
+            push!(samples, (sample=s, taxon=t, total=0., agelabel=agelabels[s], cogScore=cogscores[s]))
+        end
+    end
+
+    xs = dictionary(s=> i for (s,i) in eachrow(totals[!,[:sample,:ord]]))
+    samples.x = [xs[s] for s in samples.sample]
+    sort!(samples, :x)
+    samples
+end
+
+glutd = let geneset = Set(nko["Glutamate degradation"])
+    filt = filter(row-> row.ko in geneset, koslong)
+    samples = by(filt, [:sample, :taxon]) do sample
+        (total=sum(sample.abundance), agelabel=first(sample).agelabel)
+    end
+    @info names(samples)
+    sort!(samples, [:agelabel, :sample])
+    totals = filter(row-> isnothing(row.taxon), samples)
+    filter!(row-> !isnothing(row.taxon), samples)
+    totals.ord = invperm(sortperm(totals, [:agelabel, :total]))
+
+    spectotals = by(samples, :taxon, spectotal = :total =>sum)
+    sort!(spectotals, :spectotal)
+    topspec = Set(last(spectotals, 5).taxon)
+
+    by(samples, :sample) do df
+        df = filter(row-> !in(row.taxon, topspec), df)
+        if size(df, 1) == 0
+            1
+        else
+            other = sum(df.total)
+            sample = first(df).sample
+            push!(samples, (sample=sample, taxon="other", total=other, agelabel=agelabels[sample]))
+            1
+        end
+    end
+    push!(topspec, "other")
+    filter!(row-> row.taxon in topspec, samples)
+
+
+    for s in unique(samples.sample)
+        missing_tax = setdiff(topspec, samples[samples.sample .== s, :taxon])
+        for t in missing_tax
+            push!(samples, (sample=s, taxon=t, total=0., agelabel=agelabels[s]))
+        end
+    end
+
+    xs = dictionary(s=> i for (s,i) in eachrow(totals[!,[:sample,:ord]]))
+    samples.x = [xs[s] for s in samples.sample]
+    sort!(samples, :x)
+    samples
+end
+
+gabad = let geneset = Set(nko["GABA degradation"])
     filt = filter(row-> row.ko in geneset, koslong)
     samples = by(filt, [:sample, :taxon]) do sample
         (total=sum(sample.abundance), agelabel=first(sample).agelabel)
@@ -53,82 +137,78 @@ koslong.agelabel = [agelabels[s] for s in koslong.sample]
     totals = filter(row-> isnothing(row.taxon), samples)
     filter!(row-> !isnothing(row.taxon), samples)
     totals.ord = invperm(sortperm(totals, [:agelabel, :total]))
-    alltaxa = Set(samples.taxon)
+
+    spectotals = by(samples, :taxon, spectotal = :total =>sum)
+    sort!(spectotals, :spectotal)
+    topspec = Set(last(spectotals, 5).taxon)
+
+    by(samples, :sample) do df
+        df = filter(row-> !in(row.taxon, topspec), df)
+        if size(df, 1) == 0
+            1
+        else
+            other = sum(df.total)
+            sample = first(df).sample
+            push!(samples, (sample=sample, taxon="other", total=other, agelabel=agelabels[sample]))
+            1
+        end
+    end
+    push!(topspec, "other")
+    filter!(row-> row.taxon in topspec, samples)
+
 
     for s in unique(samples.sample)
-        missing_tax = setdiff(alltaxa, samples[samples.sample .== s, :taxon])
+        missing_tax = setdiff(topspec, samples[samples.sample .== s, :taxon])
         for t in missing_tax
             push!(samples, (sample=s, taxon=t, total=0., agelabel=agelabels[s]))
         end
     end
+
     xs = dictionary(s=> i for (s,i) in eachrow(totals[!,[:sample,:ord]]))
     samples.x = [xs[s] for s in samples.sample]
     sort!(samples, :x)
-    (samples, totals)
+    samples
 end
 
-CSV.write("/Users/ksb/Desktop/samples.csv", s)
+gabas = let geneset = Set(nko["GABA synthesis"])
+    filt = filter(row-> row.ko in geneset, koslong)
+    samples = by(filt, [:sample, :taxon]) do sample
+        (total=sum(sample.abundance), agelabel=first(sample).agelabel)
+    end
 
-##
+    sort!(samples, [:agelabel, :sample])
+    totals = filter(row-> isnothing(row.taxon), samples)
+    filter!(row-> !isnothing(row.taxon), samples)
+    totals.ord = invperm(sortperm(totals, [:agelabel, :total]))
 
-scene, layout = layoutscene()
-ax = layout[1,1] = LAxis(scene)
-p1 = barplot!(ax, Position.stack, Data(s), Group(:taxon), :x, :total)
+    spectotals = by(samples, :taxon, spectotal = :total =>sum)
+    sort!(spectotals, :spectotal)
+    topspec = Set(last(spectotals, 5).taxon)
 
-tightlimits!(ax)
-hidexdecorations!(ax)
+    by(samples, :sample) do df
+        df = filter(row-> !in(row.taxon, topspec), df)
+        if size(df, 1) == 0
+            1
+        else
+            other = sum(df.total)
+            sample = first(df).sample
+            push!(samples, (sample=sample, taxon="other", total=other, agelabel=agelabels[sample]))
+            1
+        end
+    end
+    push!(topspec, "other")
+    filter!(row-> row.taxon in topspec, samples)
 
-annbar = layout[2,1] = LAxis(scene, height=100)
-linkxaxes!(ax, annbar)
-tightlimits!(annbar)
-hidexdecorations!(annbar)
-hideydecorations!(annbar)
 
-for (l, c) in zip(unique(s.agelabel), ColorBrewer.palette("Set3", 4))
-    (start, stop) = extrema(s[s.agelabel .== l, :x])
-    poly!(annbar, Point2f0[(start-0.5,0), (stop+0.5, 0), (stop+0.5, 1), (start-0.5, 1)], color = c)
+    for s in unique(samples.sample)
+        missing_tax = setdiff(topspec, samples[samples.sample .== s, :taxon])
+        for t in missing_tax
+            push!(samples, (sample=s, taxon=t, total=0., agelabel=agelabels[s]))
+        end
+    end
+
+    xs = dictionary(s=> i for (s,i) in eachrow(totals[!,[:sample,:ord]]))
+    samples.x = [xs[s] for s in samples.sample]
+    sort!(samples, :x)
+    samples
 end
-
-p1legend= layout[1,2] = LLegend(scene, height=Auto(true), width=Auto(false),
-                            titlevisible=false, patchcolor=:transparent)
-
-for (t,p) in zip(unique(s.taxon), p1.plots)
-    push!(p1legend,
-        LegendEntry(t, MarkerElement(color = p.attributes.color[], marker = :rect, strokecolor = :black))
-        )
-end
-
-scene
-
-
-### incomplete
-
-# function plotstrat(genedf, geneset; samplecol=:sample, groupcol=:taxon,
-#                                     anncols = [:agelabel], abundcol=:abundance, groupn=5)
-#
-#     annotations = dictionary(a => )
-#
-#     filt = filter(row-> row.ko in geneset, genedf)
-#     samples = by(filt, [samplecol, groupcol]) do sample
-#             DataFrame(
-#                 :total=>sum(sample[!, abundcol])
-#                 [a => first(sample)[a] for a in anncols]...)
-#     end
-#
-#     sort!(samples, [anncols..., samplecol])
-#     totals = filter(row-> isnothing(row[groupcol]), samples)
-#     filter!(row-> !isnothing(row[groupcol]), samples)
-#     totals.ord = invperm(sortperm(totals, [anncols..., :total]))
-#     alltaxa = Set(samples.taxon)
-#
-#     for s in unique(samples[!,samplecol])
-#         missing_groups = setdiff(alltaxa, samples[samples[!,samplecol] .== g, groupcol])
-#         for mg in missing_groups
-#             push!(samples, (; samplcol=s, groupcol=mg, total=0., agelabel=agelabels[s]))
-#         end
-#     end
-#     xs = dictionary(s=> i for (s,i) in eachrow(totals[!,[:sample,:ord]]))
-#     samples.x = [xs[s] for s in samples.sample]
-#     sort!(samples, :x)
-#     (samples, totals)
-# end
