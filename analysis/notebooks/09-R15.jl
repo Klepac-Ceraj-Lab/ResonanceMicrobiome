d29f0258b707cc9667fff64c4e7e324816879669
Kevin Bonham <kevbonham@gmail.com>
2020-04-20 11:59:35 -0400

    work on R15

--- analysis/notebooks/09-R15.jl
+++ analysis/notebooks/09-R15.jl
@@ -7,15 +7,13 @@ using MultivariateStats
 using Statistics
 using ECHOAnalysis
 
-@load "analysis/figures/assets/figure1a.jld2" speciesmds allmeta speciesmdsaxes
+@load "analysis/figures/assets/taxa.jld2" speciesmds allmeta speciesmdsaxes
 @load "analysis/figures/assets/figure1b.jld2" kidsspeciesmds kidsspeciesmdsaxes allkidsmeta
 @load "analysis/figures/assets/figure1c.jld2" unirefaccessorymds ubothmeta unirefaccessorymdsaxes
 @load "analysis/figures/assets/figure1d.jld2" kidsunirefaccessorymds kidsunirefaccessorymdsaxes
 @load "analysis/figures/assets/figure1e.jld2" r2 r2m qa allpermanovas
 @load "analysis/figures/assets/figure1g.jld2" allfsea mdcors
 @load "analysis/figures/assets/s1.jld2" speciesdiffs unirefaccessorydiffs kosdiffs pfamsdiffs
-allfsea.median = map(median, allfsea.cors)
-rename!(r2, :unirefaccessory=>:accessory)
 
 # Get samples for kids under ~14 months
 
@@ -29,7 +27,7 @@ fastqs = String[]
 for (root, dirs, files) in walkdir("/lovelace/echo/analysis/engaging/")
     !occursin(r"batch\d{3}", root) && continue
     !occursin(r"kneaddata", root) && continue
-    filter!(f-> occursin(r"kneaddata\.fastq.gz$", f) && 
+    filter!(f-> occursin(r"kneaddata\.fastq.gz$", f) &&
                 sampleid(stoolsample(f)) in samples,
             files)
 
@@ -65,15 +63,15 @@ end
 #
 # ```julia
 # cd("/nobackup1/vklepacc/echo/metaphlan3/")
-# 
+#
 # fastqs = joinpath.("fastqs", readdir("fastqs"))
-# 
+#
 # sids = map(fastqs) do f
 #     m = match(r"fastqs\/(C[\w\-]+)_S\d{1,2}_kneaddata\.fastq\.gz", f)
 #     isnothing(m) && error("no match for $f")
 #     string(m.captures[1])
 # end
-# 
+#
 # for (f, i) in zip(fastqs, sids)
 #     @async run(`sbatch --export=infile=$f,sid=$i batch_job.sh`)
 # end
@@ -84,7 +82,7 @@ end
 newprofiles = readdir("/lovelace/echo/analysis/metaphlan3/profiles/", join=true)
 df = DataFrame(CSV.File(newprofiles[1], delim='\t', header=["clade", "taxid", "abundance"], select=[1,2,3], skipto=5))
 
-size(df)    
+size(df)
 df[!,4]
 names(df)
 dfs = [DataFrame(CSV.File(f, delim='\t')) for f in newprofiles]
