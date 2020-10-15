0573b3b884b8d4d7114e5979d27ab328e6a1ba5e
Kevin Bonham, PhD <kevbonham@gmail.com>
2020-10-15 13:07:48 -0400

    work on SRS data

--- notebooks/12-SRS-data.jl
+++ notebooks/12-SRS-data.jl
@@ -14,8 +14,28 @@ samplemeta = airtable_metadata()
 
 allmeta = leftjoin(unique(samplemeta), srs, on=[:subject,:timepoint])
 
-count(row-> !any(ismissing, row[["batch_16S", "SchoolageSRS::timepoint"]]), eachrow(allmeta))
-count(row-> !any(ismissing, row[["batch_16S", "PreschoolSRS::timepoint"]]), eachrow(allmeta))
+@info "16S has PreschoolSRS"
+count(row-> !any(ismissing, row[["batch_16S", "PreschoolSRS::timepoint"]]), eachrow(allmeta)) |> println
+@info "16S has SchoolageSRS"
+count(row-> !any(ismissing, row[["batch_16S", "SchoolageSRS::timepoint"]]), eachrow(allmeta)) |> println
 
-count(row-> !any(ismissing, row[["batch", "SchoolageSRS::timepoint"]]), eachrow(allmeta))
-count(row-> !any(ismissing, row[["batch", "PreschoolSRS::timepoint"]]), eachrow(allmeta))
+@info "mgx has PreschoolSRS"
+count(row-> !any(ismissing, row[["batch", "PreschoolSRS::timepoint"]]), eachrow(allmeta)) |> println
+@info "mgx has SchoolageSRS"
+count(row-> !any(ismissing, row[["batch", "SchoolageSRS::timepoint"]]), eachrow(allmeta)) |> println
+
+
+## CBCL
+
+cbcl = CSV.File(datadir("metadata", "has_CBCL.csv")) |> DataFrame
+allmeta = leftjoin(unique(samplemeta), cbcl, on=[:subject,:timepoint])
+
+@info "16S has younger CBCL"
+count(row-> !any(ismissing, row[["batch_16S", "cbclAge"]]) && row["cbclAge"] == "younger", eachrow(allmeta)) |> println
+@info "16S has older CBCL"
+count(row-> !any(ismissing, row[["batch_16S", "cbclAge"]]) && row["cbclAge"] == "older", eachrow(allmeta)) |> println
+
+@info "mgx has younger CBCL"
+count(row-> !any(ismissing, row[["batch", "cbclAge"]]) && row["cbclAge"] == "younger", eachrow(allmeta)) |> println
+@info "mgx has older CBCL"
+count(row-> !any(ismissing, row[["batch", "cbclAge"]]) && row["cbclAge"] == "older", eachrow(allmeta)) |> println
