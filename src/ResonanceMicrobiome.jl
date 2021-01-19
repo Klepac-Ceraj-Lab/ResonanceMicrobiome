5e5437773bd560643d5ab1f68f038721da63f1ee
Kevin Bonham, PhD <kevbonham@gmail.com>
2021-01-19 15:10:14 -0500

    fix datadeps

--- src/ResonanceMicrobiome.jl
+++ src/ResonanceMicrobiome.jl
@@ -1,11 +1,16 @@
 module ResonanceMicrobiome
 
+export
+    @datadep_str
+
 using Microbiome
 using CSV
 using DataFrames
 using Arrow
 using DataDeps
+import DataDeps: @datadep_str
 using LoggingExtras
+
 
 include("data.jl")
 
