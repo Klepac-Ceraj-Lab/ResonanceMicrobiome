518e6ef22251ee7f3e51d72b54154947390b39c4
Kevin Bonham, PhD <kevbonham@gmail.com>
2021-01-27 15:00:46 -0500

    finish taxonomic pcoas

--- notebooks/03_Taxonomic_profiles.jl
+++ notebooks/03_Taxonomic_profiles.jl
@@ -35,7 +35,7 @@ all_metadata.ageLabel = labelage(all_metadata)
 
 all_pco = pcoa(all_species)
 
-#-
+##-
 
 figure1 = Figure(resolution=(1200, 800));
 
@@ -55,7 +55,7 @@ kids_metadata.ageLabel = labelage(kids_metadata)
 kids_pco = pcoa(kids_species)
 
 fig1b = figure1[1,2] = Axis(figure1, title="Children", xlabel=mds_format(kids_pco, 1), ylabel=mds_format(kids_pco, 2))
-scatter!(fig1b, projection(kids_pco)[:,1] .* -1, projection(kids_pco)[:,2] .* -1,
+scatter!(fig1b, projection(kids_pco)[:,1] .* -1, projection(kids_pco)[:,2],
         color=categorical_colors(kids_metadata.ageLabel, ["mom", "1 and under", "1 to 2", "over 2", missing], colormap[[9, 2, 3, 5, 15]]))
 figure1
 
@@ -74,11 +74,13 @@ has_age_species = kids_species[:, map(!ismissing, kids_metadata.correctedAgeDays
 has_age_metadata = resonance_metadata(name.(samples(has_age_species)))
 
 has_age_metadata.ageLabel = labelage(has_age_metadata)
+has_age_metadata.shannon = vec(shannon(has_age_species))
 
-
-has_age_pco = pcoa(kids_species)
+has_age_pco = pcoa(has_age_species)
 
 fig1c = figure1[2, 1:2] = Axis(figure1, xlabel=mds_format(kids_pco, 1), ylabel="Age (years)")
 
+scatter!(fig1c, projection(has_age_pco)[:,1] .* -1, has_age_metadata.correctedAgeDays ./ 365, color=has_age_metadata.shannon, label="Shannon diversity")
 
-scatter!(fig1b, projection(kids_pco)[:,1] .* -1, has_age_metadata.correctedAgeDays, label="Shannon diversity")
+fig1c_legend = figure1[2,3] = Colorbar(figure1, halign=:left, limits=extrema(has_age_metadata.shannon), width=25, label="Shannon diversity", )
+figure1
